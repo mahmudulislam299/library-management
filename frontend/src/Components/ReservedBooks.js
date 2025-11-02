fb04e3fa1f4822f54e9371f3eb6876242e56b16f
mahmudulislam299 <mahmudulislam299@gmail.com>
2025-11-02 23:41:24 +0600

    change

--- frontend/src/Components/ReservedBooks.js
+++ frontend/src/Components/ReservedBooks.js
@@ -1,49 +1,41 @@
-import React from 'react'
-import './ReservedBooks.css'
+import React from "react";
+import "./ReservedBooks.css";
 
 function ReservedBooks() {
-    return (
-        <div className='reservedbooks-container'>
-            <h className='reservedbooks-title'>Books On Hold</h>
-            <table className='reservedbooks-table'>
-                <tr>
-                    <th>Name</th>
-                    <th>Book</th>
-                    <th>Date</th>
-                </tr>
-                <tr>
-                    <td>Anika</td>
-                    <td>Data Sturcture</td>
-                    <td>12/7/2025</td>
-                </tr>
-                <tr>
-                    <td>Diya</td>
-                    <td>Operating Systems</td>
-                    <td>10/7/2025</td>
-                </tr>
-                <tr>
-                    <td>Aisharjaa</td>
-                    <td>Computer Networks</td>
-                    <td>15/9/2025</td>
-                </tr>
-                <tr>
-                    <td>Daliya</td>
-                    <td>Database Management</td>
-                    <td>02/9/2025</td>
-                </tr>
-                <tr>
-                    <td>Nadia</td>
-                    <td>Artificial Intelligence</td>
-                    <td>21/7/2025</td>
-                </tr>
-                <tr>
-                    <td>Farhana</td>
-                    <td>Machine learning</td>
-                    <td>02/7/2025</td>
-                </tr>
-            </table>
-        </div>
-    )
+  const reservations = [
+    { name: "Anika", book: "Data Structure", date: "12/7/2025" },
+    { name: "Diya", book: "Operating Systems", date: "10/7/2025" },
+    { name: "Aisharjaa", book: "Computer Networks", date: "15/9/2025" },
+    { name: "Daliya", book: "Database Management", date: "02/9/2025" },
+    { name: "Nadia", book: "Artificial Intelligence", date: "21/7/2025" },
+    { name: "Farhana", book: "Machine Learning", date: "02/7/2025" },
+  ];
+
+  return (
+    <section className="reservedbooks-container">
+      <h2 className="reservedbooks-title">Books On Hold</h2>
+      <div className="table-wrapper">
+        <table className="reservedbooks-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Book</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reservations.map((item, index) => (
+              <tr key={index} className="table-row">
+                <td>{item.name}</td>
+                <td>{item.book}</td>
+                <td>{item.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
 }
 
-export default ReservedBooks
+export default ReservedBooks;
