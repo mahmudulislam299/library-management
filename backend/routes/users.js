8730c1ec34023d7b92a33cc07584e60fb4e8687a
mahmudulislam299 <mahmudulislam299@gmail.com>
2025-11-09 22:53:39 +0600

    dob and age removed and department added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,103 +1,123 @@
 import express from "express";
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 
-const router = express.Router()
+const router = express.Router();
 
 /* Getting user by id */
 router.get("/getuser/:id", async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id).populate("activeTransactions").populate("prevTransactions")
-        const { password, updatedAt, ...other } = user._doc;
-        res.status(200).json(other);
-    } 
-    catch (err) {
-        return res.status(500).json(err);
+  try {
+    const user = await User.findById(req.params.id)
+      .populate("activeTransactions")
+      .populate("prevTransactions");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-})
+
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    console.error("Error in /getuser/:id:", err.message);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
 
 /* Getting all members in the library */
-router.get("/allmembers", async (req,res)=>{
-    try{
-        const users = await User.find({}).populate("activeTransactions").populate("prevTransactions").sort({_id:-1})
-        res.status(200).json(users)
-    }
-    catch(err){
-        return res.status(500).json(err);
-    }
-})
+router.get("/allmembers", async (req, res) => {
+  try {
+    const users = await User.find({})
+      .populate("activeTransactions")
+      .populate("prevTransactions")
+      .sort({ _id: -1 })
+      .select("-password -updatedAt -__v"); // hide sensitive fields
+
+    res.status(200).json(users);
+  } catch (err) {
+    console.error("Error in /allmembers:", err.message);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
 
 /* Update user by id */
 router.put("/updateuser/:id", async (req, res) => {
-    if (req.body.userId === req.params.id || req.body.isAdmin) {
-        if (req.body.password) {
-            try {
-                const salt = await bcrypt.genSalt(10);
-                req.body.password = await bcrypt.hash(req.body.password, salt);
-            } catch (err) {
-                return res.status(500).json(err);
-            }
-        }
-        try {
-            const user = await User.findByIdAndUpdate(req.params.id, {
-                $set: req.body,
-            });
-            res.status(200).json("Account has been updated");
-        } catch (err) {
-            return res.status(500).json(err);
-        }
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        console.error("Error hashing password in /updateuser:", err.message);
+        return res.status(500).json({ message: "Password hashing failed" });
+      }
     }
-    else {
-        return res.status(403).json("You can update only your account!");
+
+    try {
+      await User.findByIdAndUpdate(req.params.id, { $set: req.body });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      console.error("Error in /updateuser/:id:", err.message);
+      return res.status(500).json({ message: "Server error" });
     }
-})
+  } else {
+    return res.status(403).json("You can update only your account!");
+  }
+});
 
 /* Adding transaction to active transactions list */
-router.put("/:id/move-to-activetransactions" , async (req,res)=>{
-    if(req.body.isAdmin){
-        try{
-            const user = await User.findById(req.body.userId);
-            await user.updateOne({$push:{activeTransactions:req.params.id}})
-            res.status(200).json("Added to Active Transaction")
-        }
-        catch(err){
-            res.status(500).json(err)
-        }
-    }
-    else{
-        res.status(403).json("Only Admin can add a transaction")
-    }
-})
+router.put("/:id/move-to-activetransactions", async (req, res) => {
+  if (req.body.isAdmin) {
+    try {
+      const user = await User.findById(req.body.userId);
+      if (!user) {
+        return res.status(404).json("User not found");
+      }
 
-/* Adding transaction to previous transactions list and removing from active transactions list */
-router.put("/:id/move-to-prevtransactions", async (req,res)=>{
-    if(req.body.isAdmin){
-        try{
-            const user = await User.findById(req.body.userId);
-            await user.updateOne({$pull:{activeTransactions:req.params.id}})
-            await user.updateOne({$push:{prevTransactions:req.params.id}})
-            res.status(200).json("Added to Prev transaction Transaction")
-        }
-        catch(err){
-            res.status(500).json(err)
-        }
+      await user.updateOne({ $push: { activeTransactions: req.params.id } });
+      res.status(200).json("Added to Active Transaction");
+    } catch (err) {
+      console.error("Error in /:id/move-to-activetransactions:", err.message);
+      res.status(500).json({ message: "Server error" });
     }
-    else{
-        res.status(403).json("Only Admin can do this")
+  } else {
+    res.status(403).json("Only Admin can add a transaction");
+  }
+});
+
+/* Adding transaction to previous transactions list and removing from active */
+router.put("/:id/move-to-prevtransactions", async (req, res) => {
+  if (req.body.isAdmin) {
+    try {
+      const user = await User.findById(req.body.userId);
+      if (!user) {
+        return res.status(404).json("User not found");
+      }
+
+      await user.updateOne({ $pull: { activeTransactions: req.params.id } });
+      await user.updateOne({ $push: { prevTransactions: req.params.id } });
+      res.status(200).json("Added to previous transaction");
+    } catch (err) {
+      console.error("Error in /:id/move-to-prevtransactions:", err.message);
+      res.status(500).json({ message: "Server error" });
     }
-})
+  } else {
+    res.status(403).json("Only Admin can do this");
+  }
+});
 
 /* Delete user by id */
 router.delete("/deleteuser/:id", async (req, res) => {
-    if (req.body.userId === req.params.id || req.body.isAdmin) {
-        try {
-            await User.findByIdAndDelete(req.params.id);
-            res.status(200).json("Account has been deleted");
-        } catch (err) {
-            return res.status(500).json(err);
-        }
-    } else {
-        return res.status(403).json("You can delete only your account!");
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      console.error("Error in /deleteuser/:id:", err.message);
+      return res.status(500).json({ message: "Server error" });
     }
-})
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+});
 
-export default router
+export default router;
